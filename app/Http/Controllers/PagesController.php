103630d7401adcfafdbee86a31a95b61013a11a3
Admin <5828537@gmail.com>
2019-05-05 13:52:41 -0400

    initial commit

---
+++ app/Http/Controllers/PagesController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PagesController extends Controller
+{
+    public function index() {
+        $title = 'Welcome to Laravel!';
+        return view('pages.index')->with('title', $title);
+    }
+
+    public function about() {
+        $title = 'About us...';
+        return view('pages.about')->with('title', $title);
+    }
+
+    /**
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function services(){
+        $data = array(
+            'title' => 'Services',
+            'services' => ['Web Design', 'Programming', 'SEO']
+        );
+        return view('pages.services')->with($data);
+    }
+
+}
---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,6 @@
+@extends ('layouts.app')
+
+@section('content')
+        <h1>{{$title}}</h1>
+        <p>This is the Laravel application from the "Laravel from Scratch" Youtube Series</p>
+@endsection
